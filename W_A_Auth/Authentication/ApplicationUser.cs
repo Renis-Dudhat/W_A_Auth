1ce9f8e80fc68d9ea122df66ebb6a16e63f12bcc
Renis_Dudhat_Own <renisdudhat111@gmail.com>
2022-09-08 12:31:46 +0530

    Add project files.

---
+++ W_A_Auth/Authentication/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace W_A_Auth.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ W_A_Auth/Authentication/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace W_A_Auth.Authentication
+{
+    public class Response
+    {
+        public string  Status { get; set; }
+        public string Message { get; set; }
+    }
+}
